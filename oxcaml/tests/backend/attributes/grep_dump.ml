1ceec3a13ed320bdb6b0b3473a485b12bf0d0b32
Samuel Hym <samuel@tarides.com>
2026-02-18 15:42:43 +0100

    Support both name-mangling schemes in backend/attributes test

--- oxcaml/tests/backend/attributes/grep_dump.ml
+++ oxcaml/tests/backend/attributes/grep_dump.ml
@@ -3,7 +3,12 @@ let is_interesting_line line =
     (fun prefix -> String.starts_with ~prefix line)
     ["use_regalloc="; "regalloc_params="]
 
-let function_name = Str.regexp "caml\\(.*\\)_[0-9]+_[0-9]+\\(_code\\)?"
+let function_name =
+  match Sys.argv.(1) with
+  | "flat" -> Str.regexp "caml\\(.*\\)_[0-9]+_[0-9]+\\(_code\\)?"
+  | "structured" -> Str.regexp "_Caml\\(.*\\)_[0-9]+_[0-9]+\\(_code\\)?"
+  | unsupported ->
+    failwith (Printf.sprintf "Unsupported name-mangling scheme: %S" unsupported)
 
 let remove_suffix line =
   match Str.string_match function_name line 0 with
@@ -11,7 +16,7 @@ let remove_suffix line =
   | true -> Str.matched_group 1 line
 
 let () =
-  let chan = open_in Sys.argv.(1) in
+  let chan = open_in Sys.argv.(2) in
   try
     let prev_line = ref "" in
     while true do
